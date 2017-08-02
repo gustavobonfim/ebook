1bca10e2cadf0994573066eb69b4bbcbade2e0a6
gustavobonfim <bonfimoliveira@gmail.com>
2017-08-02 16:02:36 -0300

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'dashboard', to: 'pages#dashboard'
+  get 'clinicas', to: 'pages#clinicas'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
